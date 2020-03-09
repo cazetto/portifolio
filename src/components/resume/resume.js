b23ded4498b6482725c90ddb80b1eba2769b3498
André Pesci Cazetto <cazetto.andre@gmail.com>
2020-03-09 11:00:43 -0300

    Change CV

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -8,7 +8,7 @@ const Resume = () => ({
           <h1 class="title has-text-grey-darker p-b-md">
             Download My Resume
           <h1>
-          <a href="https://drive.google.com/file/d/1DGVKcHge1GuIng1RSmLFraHpnTVrgEEi/view?usp=sharing" target="_blank" class="icon is-large has-text-black m-l-md" m-r-md>
+          <a href="https://docs.google.com/document/d/1itUVgwJ0T2jdbZfskHm_2SPeXlDRhe56CFKRVz3RsOA/edit?usp=sharing" target="_blank" class="icon is-large has-text-black m-l-md" m-r-md>
             <i class="fas fa-file-alt fa-3x"></i>
           </a>
         </div>
